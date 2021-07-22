1ca596f82d0c5abfac34da5c0cd07aa53e5141c4
Rod Ball <Rodericrlb@gmail.com>
2021-07-22 10:35:00 -0400

    init

--- src/app.js
+++ src/app.js
@@ -5,12 +5,34 @@ const app = express();
 
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
+
 app.use(express.static(path.join(__dirname, '/public/')));
 
+const accountData= fs.readFileSync('src/json/accounts.json', {encoding:'utf8'});
+const accounts= JSON.parse(accountData);
+const userData= fs.readFileSync('src/json/users.json', {encoding:'utf8'});
+const users= JSON.parse(userData);
 
 app.get('/', function(req, res){
-    res.render('index',{title: 'Index'});
+    res.render('index',{title: 'Account Summary', accounts: accounts});
 }); 
+
+app.get('/savings', function(req, res){
+    res.render('account',{account: accounts.savings});
+}); 
+
+app.get('/credit', function(req, res){
+    res.render('account',{title: 'Account Credit Summary', account: accounts.credit});
+}); 
+
+app.get('/checking', function(req, res){
+    res.render('account',{title: 'Account Checking Summary', account: accounts.checking});
+}); 
+
+app.get('/profile', function(req, res){
+    res.render('profile',{user: users[0]});
+}); 
+
 
 app.listen(3000, function(){
     console.log(`PS Project Running on port 3000`);
